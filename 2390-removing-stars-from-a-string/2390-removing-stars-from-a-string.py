4cff8f7ec16c49c8d01c044e13e5d8de0b7d4b68
Eden Asamere <126487966+EdenAsamere@users.noreply.github.com>
2023-10-18 08:20:49 -0700

    Time: 192 ms (53.19%), Space: 17.7 MB (80.72%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack =[]
+        for i in s:
+            if i.isalpha():
+                stack.append(i)
+            else:
+                stack.pop()
+        return ''.join(stack)
+            
+        
