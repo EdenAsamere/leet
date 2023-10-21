29094a909dc73dc050c1bb308455e427719540cd
Eden Asamere <126487966+EdenAsamere@users.noreply.github.com>
2023-10-21 02:53:00 -0700

    Time: 444 ms (77.66%), Space: 29.4 MB (38.91%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -3,14 +3,15 @@ class Solution:
         skill.sort()
         s,e = 0,len(skill)-1
         chemistry = 0
-        tskill = set()
+        tskill = skill[s]+skill[e]
         while s < e:
-            tskill.add(skill[s]+skill[e])
-            if len(tskill)>1:
+            diff = skill[s]+skill[e] 
+            if tskill != diff:
                 return -1
             else:
                 chemistry += (skill[s]*skill[e])
                 s+=1
                 e-=1
+                
         return chemistry
         
